f726e80bd157b28332a570925a6ddd68318acbac
andrewkizito <andrewkizito54@gmail.com>
2025-10-08 16:34:13 +0300

    feat: Update storage name to use environment-specific naming convention

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,7 +1,8 @@
 import { defineStorage } from "@aws-amplify/backend";
+import { isSandbox } from "../utils";
 
 export const storage = defineStorage({
-  name: "kalevmedia",
+  name: isSandbox ? "kalev-media-files-sandbox" : "kalev-media-files",
   access: (allow) => ({
     "public/*": [
       allow.guest.to(["read"]),
