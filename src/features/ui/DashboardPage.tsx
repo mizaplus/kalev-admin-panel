8c04f8bca0f69600520b185975906445f97a1e87
andrewkizito <andrewkizito54@gmail.com>
2025-10-20 12:51:40 +0300

    feat: add ScrollArea component and integrate WebsiteStatus in DashboardPage
    - Created a new ScrollArea component for enhanced scrolling functionality.
    - Added WebsiteStatus component to display website status with performance metrics.
    - Updated DashboardPage to include the new WebsiteStatus component.

--- src/features/ui/DashboardPage.tsx
+++ src/features/ui/DashboardPage.tsx
@@ -1,10 +1,12 @@
 import DashboardProvider from "../domain/context/dashboard";
 import AnalyticsCards from "../useCases/dashboard/AnalyticsCards";
+import WebsiteStatus from "../useCases/dashboard/WebsiteStatus";
 
 export default function DashboardPage() {
   return (
     <DashboardProvider>
       <AnalyticsCards />
+      <WebsiteStatus />
     </DashboardProvider>
   );
 }
---
+++ src/features/useCases/dashboard/WebsiteStatus.tsx
@@ -0,0 +1,41 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import "@aws-amplify/ui-react/styles.css";
+import { useDashboardContext } from "@/features/domain/context/dashboard";
+import { resolveMediaUrl } from "@/lib/media";
+
+const WebsiteStatus = () => {
+  const { data } = useDashboardContext();
+  const record = data?.records[0];
+
+  if (!record) {
+    return null;
+  }
+
+  const page = record.website.pages[0];
+
+  return (
+    <Card className="rounded-sm mt-5">
+      <CardHeader>
+        <CardTitle className="text-lg">Website Status</CardTitle>
+        <CardDescription>
+          Displays the current status of the website including uptime and
+          performance metrics.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ScrollArea className="w-full rounded-md border h-[500px] max-w-5xl">
+          <img src={resolveMediaUrl(page.screenshot)} alt="" />
+        </ScrollArea>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default WebsiteStatus;
