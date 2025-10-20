6241c3617f9bc220ebc1ba07a8e1b91ee08126bf
andrewkizito <andrewkizito54@gmail.com>
2025-10-20 13:36:29 +0300

    feat: add webpage selection functionality to WebsiteStatus component

--- src/features/useCases/dashboard/WebsiteStatus.tsx
+++ src/features/useCases/dashboard/WebsiteStatus.tsx
@@ -9,16 +9,32 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import "@aws-amplify/ui-react/styles.css";
 import { useDashboardContext } from "@/features/domain/context/dashboard";
 import { resolveMediaUrl } from "@/lib/media";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
 
 const WebsiteStatus = () => {
   const { data } = useDashboardContext();
   const record = data?.records[0];
+  const [activeIndex, setActiveIndex] = useState(0);
 
   if (!record) {
     return null;
   }
 
-  const page = record.website.pages[0];
+  const page = record.website.pages[activeIndex];
+
+  const pageOptions = record.website.pages.map((page, index) => ({
+    label: page.url.replace(/^(https?:\/\/)?(www\.)?/, ""),
+    value: index.toString(),
+  }));
 
   return (
     <Card className="rounded-sm mt-5">
@@ -30,6 +46,26 @@ const WebsiteStatus = () => {
         </CardDescription>
       </CardHeader>
       <CardContent>
+        <div className="my-4">
+          <Select
+            value={activeIndex.toString()}
+            onValueChange={(value) => setActiveIndex(parseInt(value))}
+          >
+            <SelectTrigger className="w-[500px] !text-sm">
+              <SelectValue placeholder="Select a webpage" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Website Pages</SelectLabel>
+                {pageOptions.map((option) => (
+                  <SelectItem key={option.value} value={option.value}>
+                    {option.label}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
         <ScrollArea className="w-full rounded-md border h-[800px]">
           <img src={resolveMediaUrl(page.screenshot)} alt="" />
         </ScrollArea>
